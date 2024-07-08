6081e638015c85a9907027b683b5b797c8d3672d
Guillaume Bogard <guillaume.bogardcoquard@gmail.com>
2024-07-08 11:29:12 -0700

    tests: update and add tests for Arguments class
    Renamed existing getArguments test to getArgumentsOneArgument
    
    Added new tests for multiple arguments, no arguments, empty arguments, and nullptr arguments

--- tests/RocketEngine/App/Arguments.cpp
+++ tests/RocketEngine/App/Arguments.cpp
@@ -3,7 +3,7 @@
 #include "RocketEngine/App/Arguments.hh"
 
 // NOLINTNEXTLINE
-TEST(Arguments, getArguments)
+TEST(Arguments, getArgumentsOneArgument)
 {
     char* const argv[] = {(char*)"./app", (char*)"--help"}; // NOLINT
     int const argc = 2;
@@ -12,4 +12,48 @@ TEST(Arguments, getArguments)
     EXPECT_EQ(arguments.size(), argc);
     EXPECT_EQ(arguments[0], "./app");
     EXPECT_EQ(arguments[1], "--help");
+}
+
+// NOLINTNEXTLINE
+TEST(Arguments, getArgumentsMultipleArguments)
+{
+    char* const argv[] = {(char*)"./app", (char*)"--help", (char*)"--verbose"}; // NOLINT
+    int const argc = 3;
+    auto const arguments = rocketengine::app::getArguments(argc, argv); // NOLINT
+
+    EXPECT_EQ(arguments.size(), argc);
+    EXPECT_EQ(arguments[0], "./app");
+    EXPECT_EQ(arguments[1], "--help");
+    EXPECT_EQ(arguments[2], "--verbose");
+}
+
+// NOLINTNEXTLINE
+TEST(Arguments, getArgumentsNoArguments)
+{
+    char* const argv[] = {(char*)"./app"}; // NOLINT
+    int const argc = 1;
+    auto const arguments = rocketengine::app::getArguments(argc, argv); // NOLINT
+
+    EXPECT_EQ(arguments.size(), argc);
+    EXPECT_EQ(arguments[0], "./app");
+}
+
+// NOLINTNEXTLINE
+TEST(Arguments, getArgumentsEmptyArguments)
+{
+    char* const argv[] = {}; // NOLINT
+    int const argc = 0;
+    auto const arguments = rocketengine::app::getArguments(argc, argv); // NOLINT
+
+    EXPECT_EQ(arguments.size(), argc);
+}
+
+// NOLINTNEXTLINE
+TEST(Arguments, getArgumentsNullptr)
+{
+    char* const* argv = nullptr;
+    int const argc = 0;
+    auto const arguments = rocketengine::app::getArguments(argc, argv); // NOLINT
+
+    EXPECT_EQ(arguments.size(), argc);
 }
